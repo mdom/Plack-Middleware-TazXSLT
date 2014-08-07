3b0b77783b3af56a50c9be8b2c635583d60b02de
Mario Domgoergen <mdom@taz.de>
2014-08-07 16:03:36 +0200

    no README in Manifest before uploading to cpan

--- lib/Plack/Middleware/TazXSLT.pm
+++ lib/Plack/Middleware/TazXSLT.pm
@@ -19,7 +19,7 @@ use Plack::Util;
 
 use constant TAZ_XSLT_PROFILE => $ENV{TAZ_XSLT_PROFILE};
 
-our $VERSION = '0.53';
+our $VERSION = '0.54';
 
 sub HTTP::Response::to_psgi {
     my ($self) = @_;
